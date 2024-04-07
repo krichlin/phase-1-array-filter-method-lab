8e2f5ed961e7cc5edb626fabe1861d3df7da3a18
Ken Richlin <krichlin@gmail.com>
2024-04-07 11:47:25 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+function findMatching(names_Array, name) {
+    return names_Array.filter( 
+        (isMatching) => isMatching.toLowerCase() === name.toLowerCase()
+    );
+}
+
+function fuzzyMatch(names_Array, name) {
+    return names_Array.filter(
+        (isMatching) => isMatching.toLowerCase().indexOf(name.toLowerCase()) === 0
+    );
+}
+
+function matchName(names_Array, name) {
+    return names_Array.filter(
+        (record) => record.name == name
+
+    );
+}
